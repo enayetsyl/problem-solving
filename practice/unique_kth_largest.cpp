83604904bf52bbec126df6eb8d862d1716cb73db
enayetsyl <enayetflweb@gmail.com>
2025-03-05 17:56:04 +0600

    kth largest element find using vector

--- practice/unique_kth_largest.cpp
+++ practice/unique_kth_largest.cpp
@@ -2,38 +2,71 @@
 
 using namespace std;
 
-int main()
-{
+// Using vector and set
+// int main()
+// {
+//   int n, k;
+
+//   cin >> n >> k;
+
+//   set<int> s;
+//   // vector <int> v;
+//   for (int i = 0; i < n; i++)
+//   {
+//     int x;
+//     cin >> x;
+//     s.insert(x);
+//   }
+
+//   // for(auto i : s){
+//   //   // cout << i<< endl;
+//   //   v.push_back(i);
+//   // }
+
+//   if(k > s.size()){
+//     cout << "-1" <<endl;
+//     return 0;
+//   }
+
+//   // sort(v.begin(), v.end(), greater<int>());
+//   auto it = s.rbegin();
+//   advance(it, k-1);
+//   cout << *it<<endl;
+
+
+//   // cout << v[k-1] << endl;
+
+//   return 0;
+// }
+
+// using vector only
+
+ int main (){
   int n, k;
 
   cin >> n >> k;
 
-  set<int> s;
-  // vector <int> v;
-  for (int i = 0; i < n; i++)
-  {
+  vector<int> v;
+
+  for(int i =0; i<n; i++){
     int x;
     cin >> x;
-    s.insert(x);
+    v.push_back(x);
   }
 
-  // for(auto i : s){
-  //   // cout << i<< endl;
-  //   v.push_back(i);
-  // }
-
-  if(k > s.size()){
-    cout << "-1" <<endl;
+  if(k>v.size()){
+    cout << "-1"<<endl;
     return 0;
   }
 
-  // sort(v.begin(), v.end(), greater<int>());
-  auto it = s.rbegin();
-  advance(it, k-1);
-  cout << *it<<endl;
+  sort(v.begin(), v.end(), greater<int>());
+
+  v.erase(unique(v.begin(),v.end()),v.end());
+
+  cout << v[k-1] << endl;
 
 
-  // cout << v[k-1] << endl;
+
 
   return 0;
-}
+ }
