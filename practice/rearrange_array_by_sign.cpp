6b241683786c6653fb59a0f208ca85fc90ddf0eb
enayetsyl <enayetflweb@gmail.com>
2025-03-06 19:00:22 +0600

    rearrange array by sign optimize solution

--- practice/rearrange_array_by_sign.cpp
+++ practice/rearrange_array_by_sign.cpp
@@ -2,28 +2,56 @@
 
 using namespace std;
 
+// Solution with extra space
+// int main(){
+//   int n;
+//   cin >> n;
+//   vector<int> v;
+//   vector<int> p;
+//   vector<int> ne;
+
+//   for(int i=0; i<n; i++){
+//     int x;
+//     cin >> x;
+//     if(x>0){
+//       p.push_back(x);
+//     }else if(x<0){
+//       ne.push_back(x);
+//     }
+//   }
+
+//   for (int i=0; i<p.size(); i++){
+//     v.push_back(p[i]);
+//     v.push_back(ne[i]);
+//   }
+//   for (int i=0; i<v.size(); i++){
+//     cout << v[i] << " ";
+//   }
+
+//   return 0;
+// }
+
+// Solution without extra space
 int main(){
   int n;
   cin >> n;
-  vector<int> v;
-  vector<int> p;
-  vector<int> ne;
+  vector<int> v(n);
 
+  int p = 0; 
+  int ne = 1;
   for(int i=0; i<n; i++){
     int x;
     cin >> x;
     if(x>0){
-      p.push_back(x);
-    }else if(x<0){
-      ne.push_back(x);
+      v[p] = x;
+      p += 2;
+    } else {
+      v[ne] = x;
+      ne  +=2;
     }
   }
 
-  for (int i=0; i<p.size(); i++){
-    v.push_back(p[i]);
-    v.push_back(ne[i]);
-  }
-  for (int i=0; i<v.size(); i++){
+  for(int i=0; i<n; i++){
     cout << v[i] << " ";
   }
 
