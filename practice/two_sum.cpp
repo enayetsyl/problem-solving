1664c1e2cc03e7e6473837cfb4570d743a006401
enayetsyl <enayetflweb@gmail.com>
2025-03-09 19:11:05 +0600

    two sum with unordered map

--- practice/two_sum.cpp
+++ practice/two_sum.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main(){
+  vector<int> nums = {3, 2, 4};
+  int target = 6;
+
+  unordered_map<int, int> mp;
+
+  for(int i =0; i<nums.size(); i++){
+    int complement = target - nums[i];
+
+    if(mp.find(complement) != mp.end()){
+      cout << mp[complement] <<"," << i;
+      return 0;
+    }
+
+    mp[nums[i]] = i;
+  }
+
 
 
   return 0; 
